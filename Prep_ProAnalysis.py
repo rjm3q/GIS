d1c1666dcec84594af15c352ea189b5286a9df54
rjm3q <robert.mabry86@gmail.com>
2019-07-30 20:14:32 -0500

    del

--- Prep_ProAnalysis.py
+++
@@ -1,5 +0,0 @@
-Analyzing Python script C:\Users\robert.mabry\Desktop\TRITECH_Prep.py for use within ArcGISPro:
-WARNING 001683: Found Python 2 to 3 errors: Line 1005:             if field.name <> "NAME": ->             if field.name != "NAME":
-Line 1038:             if (field.name <> "Neighborhood"): ->             if (field.name != "Neighborhood"):
-Line 1258:         print "Feature Field Map Not Found For Feature \""+feature+"\"" ->         print("Feature Field Map Not Found For Feature \""+feature+"\"")
-Line 1583:             if row[2].strip() <> '': ->             if row[2].strip() != '': within script C:\Users\robert.mabry\Desktop\TRITECH_Prep.py
